7c1ae4b189b64e48916160590d9af33f72df119d
thisisaj1999 <atuljoshi94170@gmail.com>
2022-02-17 17:06:12 +0530

    minor changes done to again button and highscore added.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+let highscore = 0;
+// document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -23,6 +24,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').style.width = '30rem';
 
     document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   }
 
   // When guess is too high
@@ -54,7 +60,12 @@ document.querySelector('.check').addEventListener('click', function () {
 
 // Again button
 document.querySelector('.again').addEventListener('click', function () {
-  document.querySelector('.guess');
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.number').textContent = '?';
+
+  document.querySelector('.guess').value = '';
 
   document.querySelector('.message').textContent = `Start guessing...`;
 
